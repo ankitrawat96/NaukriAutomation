73ff1502c6d487047cff53051b0a61aa3bb3a276
Ankit <Ankittest1996@gmail.com>
2025-07-28 16:02:55 +0530

    TestData

--- src/test/java/DemoCases/NaukriSalaryUpdater.java
+++ src/test/java/DemoCases/NaukriSalaryUpdater.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class NaukriSalaryUpdater {
-
+///b2a781683fd04b1a93436767aced57b3
      @Test
      public void noticeUpdate(){
 
