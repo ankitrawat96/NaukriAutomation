d19cc2fc0555b06e4aed5d4a96f8f3c33b55244e
Ankit Rawat <ankitrawat@Ankits-MacBook-Air.local>
2025-08-25 09:19:50 +0530

    browserUpdate

---
+++ src/test/java/DemoCases/DriverFactory.java
@@ -0,0 +1,33 @@
+package DemoCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class DriverFactory {
+    private static WebDriver driver;
+
+    public static WebDriver createDriver() {
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
